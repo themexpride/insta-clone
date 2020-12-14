75073023f94834dd81a2b7a5e1d548d292e729d1
themexpride <59406376+themexpride@users.noreply.github.com>
2020-12-13 18:06:54 -0800

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,5 @@
 module.exports={
-    MONGOURI:"mongodb+srv://themexpride:Imtex831@cluster0.6xbea.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET:"Fd9hQh129MXtYDeCUbH6",
-    SENDGRID_API:"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"
+    MONGOURI: "",
+    JWT_SECRET:"",
+    SENDGRID_API:""
 }
