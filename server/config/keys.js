0f57effb53a3071e1b9e7c64413722a8f0449a8f
themexpride <59406376+themexpride@users.noreply.github.com>
2020-12-13 18:05:53 -0800

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -3,9 +3,3 @@ if(process.env.NODE_ENV==='production'){
 }else{
     module.exports = require('./dev')
 }
-/*
-module.exports={
-    MONGOURI:"mongodb+srv://themexpride:Imtex831@cluster0.6xbea.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET:"Fd9hQh129MXtYDeCUbH6"
-}
-*/
