b34084ba531908b749f42881ef6126b442135fb4
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2022-12-07 22:27:57 -0800

    v2 edit
    Making sure all files are uploaded

---
+++ v2/client/src/App.jsx
@@ -0,0 +1,7 @@
+import Navbar from './components/Navbar.jsx'
+
+export default function App() {
+  return (
+    <Navbar />
+  )
+}
---
+++ v2/client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return(
+        <div class="grid-x">
+            <div class="cell light-gray">
+                <ul class="menu">
+                    <li><a>One</a></li>
+                    <li><a>Two</a></li>
+                    <li><a>Three</a></li>
+                    <li><a>Four</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
