cd0083c0905de422c4dfb0e7ed0f9ab8f4c1f33e
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2022-12-07 22:40:25 -0800

    removed v1
    Finally got rid of v1 due to dependency issues noted by Github.

--- v1/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    MONGOURI: "",
-    JWT_SECRET:"",
-    SENDGRID_API:""
-}
--- v1/config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    MONGOURI:process.env.MONGOURI,
-    JWT_SECRET:process.env.JWT_SECRET,
-    SENDGRID_API:process.env.SENDGRID_API,
-    EMAIL:process.env.EMAIL
-}
