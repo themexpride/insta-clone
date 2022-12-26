ecff56e967a37a2477a74e4cbabf751658772705
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2022-12-25 22:33:13 -0800

    Update keys

--- v2/server/keys.js
+++ v2/server/keys.js
@@ -1,6 +1,8 @@
 if (process.env.NODE_ENV === "production") {
+    console.log("Production")
     module.exports = require('./config/prod.js');
 }
 else {
+    console.log("Not Production")
     module.exports = require('./config/dev.js')
 }
