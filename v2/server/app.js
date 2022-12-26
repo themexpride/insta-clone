15382a0dc7eba03ae4a69f809fb5cc118970513c
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2022-12-26 02:24:43 -0800

    Update app.js

--- v2/server/app.js
+++ v2/server/app.js
@@ -26,13 +26,12 @@ app.use(require("./routes/auth.js"));
 app.use(require("./routes/posting.js"));
 app.use(require("./routes/user.js"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static('../client/build'));
-  const path = require('path');
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
-  })
-}
+app.use(express.static('../client/build'));
+const path = require('path');
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+})
+
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
