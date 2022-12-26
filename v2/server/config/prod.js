acf22852ce792533dce1d898e33353af5dfd3564
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2022-12-25 23:07:20 -0800

    Edited env

--- v2/server/config/prod.js
+++ v2/server/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGOURI:process.env.MONGOURI,
-    JWT_SECRET:process.env.JWT_SECRET,
-    SENDINBLUE:process.env.SENDINBLUE,
-}
+  MONGOURI: process.env.MONGOURI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  SENDINBLUE: process.env.SENDINBLUE,
+};
