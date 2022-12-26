8c0d02bbf4dd81fe86870ce848c520d80ace50c8
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2022-12-26 00:35:27 -0800

    Update keys.js

--- v2/server/keys.js
+++ v2/server/keys.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
-fs.access('./config/dev.js', constants.F_OK, (err) => {
+fs.access('./config/dev.js', (err) => {
     err ? module.exports = require('./config/prod.js') : module.exports = require('./config/dev.js');
 });
