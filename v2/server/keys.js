42d6344bccfaac6ae45c566d3df3f57a9f1f0991
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2022-12-26 02:23:23 -0800

    Update keys.js

--- v2/server/keys.js
+++ v2/server/keys.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_DEVV === 11) {
-    module.exports = require('./config/prod.js')
-}
-else {
-    module.exports = require('./config/dev.js')
-}
+module.exports = {
+    MONGOURI: 'mongodb+srv://themexpride:$$$Imtex831$$$@cluster0.6xbea.mongodb.net/?retryWrites=true&w=majority',
+    JWT_SECRET: '62820d87ce9287c3444297b4a206ed89b206544cb997ff98990c948ea0d3eb',
+    SENDINBLUE: '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'
+  };
