7a6572df5ff06a53009c73609428e628ea5e0ae7
Martin Rios-Cardenas <59406376+themexpride@users.noreply.github.com>
2023-01-03 18:12:03 -0800

    found bug

--- v2/server/config/dev.js
+++ v2/server/config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGOURI: process.env.MONGOURI,
-    JWT_SECRET: process.env.JWT_SECRET,
-    SENDINBLUE: process.env.SENDINBLUE
-};
+    MONGOURI: 'mongodb+srv://themexpride:$$$Imtex831$$$@cluster0.6xbea.mongodb.net/?retryWrites=true&w=majority',
+    JWT_SECRET: '62820d87ce9287c3444297b4a206ed89b206544cb997ff98990c948ea0d3eb',
+    SENDINBLUE: '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'
+  };
--- v2/server/keys.js
+++ v2/server/keys.js
@@ -1,5 +1,6 @@
-module.exports = {
-    MONGOURI: '',
-    JWT_SECRET: '',
-    SENDINBLUE: ''
-  };
+if (process.env.NODE_DEVV === 11) {
+    module.exports = require('./config/prod.js')
+}
+else {
+    module.exports = require('./config/dev.js')
+}
